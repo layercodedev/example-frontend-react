3baed7780f2a05d690ddc7615e27d897b12c38e6
Hanan <hanan@rebase.ventures>
2025-05-08 16:03:39 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import VoiceAgent from "./ui/VoiceAgent";
+
+function App() {
+  return (
+    <div className="w-full min-h-[80vh] flex items-center justify-center">
+      <VoiceAgent />
+    </div>
+  );
+}
+
+export default App;
